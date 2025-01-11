51d8cff177a61dd071661e7bf4a8f97cdfb883bd
Karthik Yadav <mail@karthikyadav.com>
2025-01-12 00:31:07 +0545

    created Buttons on Home screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,16 +1,27 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Button, Alert, StyleSheet, Pressable } from "react-native";
 
 const Home = () => {
     return (
         <SafeAreaView>
-            <View>
-                <Text>Continue</Text>
+        <View style={styles.button}>
+            <Button title="Continue"></Button>
+        </View>
+            <View style={styles.button}>
+                <Button title="New Game"></Button>
             </View>
-            <View>
-                <Text>New Game</Text>
+            <View style={styles.button}>
+                <Button title="Creator" color={'gold'}></Button>
             </View>
         </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    button: {
+        width: '75%',
+        padding: 10,
+        alignSelf: 'center',
+    },
+});
 
 export default Home;
