2cb052e9f53794720dcf8e235f123dbc216de698
Karthik Yadav <mail@karthikyadav.com>
2025-01-11 13:07:20 +0545

    created Home screen

---
+++ screens/Home.tsx
@@ -0,0 +1,16 @@
+import { View, Text, SafeAreaView } from "react-native";
+
+const Home = () => {
+    return (
+        <SafeAreaView>
+            <View>
+                <Text>Continue</Text>
+            </View>
+            <View>
+                <Text>New Game</Text>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default Home;
